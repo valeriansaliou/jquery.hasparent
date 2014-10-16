acbf27da9c577545db3a3aca315106f238f3fb2e
Valérian Saliou <valerian@valeriansaliou.name>
2014-10-16 15:08:23 +0200

    Finish
    Signed-off-by: Valérian Saliou <valerian@valeriansaliou.name>

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,49 +25,9 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
 
 
-  // Map tasks
-  var GRUNT_TASKS_TEST = {
-    all: [['lint',0]]
-  };
-
-  var GRUNT_TASKS_LINT = {
-    js: [['jshint',0]]
-  };
-
-
   // Register tasks
   grunt.registerTask('default', function() {
-    return grunt.warn('Usage:' + '\n\n' + 'test - grunt test' + '\n\n');
-  });
-
-  grunt.registerTask('test', function() {
-    for(var t in GRUNT_TASKS_TEST) {
-      for(var i in GRUNT_TASKS_TEST[t]) {
-        grunt.task.run(GRUNT_TASKS_TEST[t][i][0] + (GRUNT_TASKS_TEST[t][i][1] ? (':' + t) : ''));
-      }
-    }
-  });
-
-  grunt.registerTask('lint', function(t) {
-    var lint_t_all = [];
-
-    if(t === null) {
-      for(var _t in GRUNT_TASKS_LINT) {
-        lint_t_all.push(_t);
-      }
-    } else if(typeof GRUNT_TASKS_LINT[t] != 'object') {
-      return grunt.warn('Invalid lint target name.\n');
-    } else {
-      lint_t_all.push(t);
-    }
-
-    for(var c in lint_t_all) {
-      t = lint_t_all[c];
-
-      for(var i in GRUNT_TASKS_LINT[t]) {
-        grunt.task.run(GRUNT_TASKS_LINT[t][i][0] + (GRUNT_TASKS_LINT[t][i][1] ? (':' + t) : ''));
-      }
-    }
+    return grunt.warn('Usage:' + '\n\n' + 'jshint - grunt jshint' + '\n\n');
   });
 
 };
